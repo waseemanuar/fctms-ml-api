dcb7e7ab36cd651d2088ef0e9b6b00188aa91fb9
waseemanuar <156840103+waseemanuar@users.noreply.github.com>
2025-06-29 15:18:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -76,4 +77,5 @@ def predict():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True)
